b454cb6c9146a29b0a6cce67f60967b0e3e29b08
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-09 15:58:10 -0500

    Main Method App

---
+++ Main Method App/Main Method App/Method.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Method_App
+{
+    class Method
+    {
+        public int Operation(int num1, int num2 = 2)// class that is being called into the program.cs
+        {
+            int result = (num1 * 7) / num2;
+            return result;
+        }
+    }
+}
