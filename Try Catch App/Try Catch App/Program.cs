6e3fdfb5792fd262a75ab00246cc9f6fe5e0d733
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-09 13:03:12 -0500

    Try Catch App

---
+++ Try Catch App/Try Catch App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Try_Catch_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>
+            {
+                1,
+                2,
+                7,
+                4,
+                9,
+                8,
+                3,
+                5,
+                6
+            };
+
+            try
+            {
+                Console.WriteLine("Enter a number to divide the list by...");
+                int num = Convert.ToInt32(Console.ReadLine());
+                foreach (int i in intList)
+                {
+                    int dividend = i / num;
+                    Console.WriteLine(i + " divided by " + num + " equals " + dividend);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Program has completed!");
+            Console.Read();
+        }
+    }
+}
