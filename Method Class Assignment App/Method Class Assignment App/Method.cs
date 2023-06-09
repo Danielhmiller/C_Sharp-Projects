372640c994074567a6c9d8f130cadf2e4c39a68b
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-09 17:10:11 -0500

    Method Class Assignment

---
+++ Method Class Assignment App/Method Class Assignment App/Method.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment_App
+{
+    class Method
+    {
+        // Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
+        public void Methodclass(int num)
+        {
+            // Entered number, divided by two.
+            int result = num / 2;
+
+            // Display the output to the screen.
+            Console.WriteLine("\n" + num + " / 2 = " + result);
+        }
+
+        // Create a method with output parameters.
+        public void OutputMethod(out int num2)
+        {
+            num2 = 10;
+            Console.WriteLine("\n" + num2);
+        }
+
+        // Overload a method.
+        public void Methodclass(int num3, int num4 = 5)
+        {
+            int result = (num3 / 2) * num4;
+            Console.WriteLine("\n" + num3 + " * " + num4 + " = " + result);
+        }
+    }
+
+    // Declare a class to be static.
+    static class StatClass
+    {
+        public static void Method2()
+        {
+            Console.WriteLine("\nThis text comes from a static method.");
+        }
+    }
+}
---
+++ Method Class Assignment App/Method Class Assignment App/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Method_Class_Assignment_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the Main() method, instantiate that class.
+            Method method = new Method();
+
+            // Have the user enter a number.
+            Console.WriteLine("Input an integer.\n");
+            int chosenInt = Convert.ToInt32(Console.ReadLine());
+
+            // Call the method on that number.
+            method.Methodclass(chosenInt);
+
+            // Have the user enter 2 numbers.
+            Console.WriteLine("\nInput an integer.\n");
+            int chosenInt2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n input another integer.\n");
+            int chosenInt3 = Convert.ToInt32(Console.ReadLine());
+
+            // Call the method on that number(Overloaded).
+            method.Methodclass(chosenInt2, chosenInt3);
+
+            // Call the method on that number(Output).
+            method.OutputMethod(out int num2);
+
+            // Call a method from a static class.
+            StatClass.Method2();
+
+            Console.Read();
+        }
+    }
+}
