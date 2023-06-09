8d4286ca3951968df7706f5e26507f78ef6ef138
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-09 15:48:26 -0500

    Method App

---
+++ Method App/Method App/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_App
+{
+    class Class1// the class below is the formula that is being called into the program.cs to do those calculations
+    {
+        public int Add(int num1)
+        {
+            int result1 = num1 + 25;
+            return result1;
+        }
+        public int Multiply(int num2)
+        {
+            int result2 = num2 * 5;
+            return result2;
+        }
+        public int Divide(int num3)
+        {
+            int result3 = num3 / 2;
+            return result3;
+        }
+    }
+}
---
+++ Method App/Method App/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number that you want to perform the math operations on below.\n");
+            int chosenNum = Convert.ToInt32(Console.ReadLine());// the class is converting to integers
+
+            Class1 method = new Class1();
+            //the console write lines are adding, multiplying and dividing whatever integer is typed in plus the values inserted below
+            Console.WriteLine("\n" + chosenNum + " + 45 = " + method.Add(chosenNum));
+            Console.WriteLine("\n" + chosenNum + " * 7 = " + method.Multiply(chosenNum));
+            Console.WriteLine("\n" + chosenNum + " / 3 = " + method.Divide(chosenNum));
+
+            Console.Read();
+        }
+    }
+}
