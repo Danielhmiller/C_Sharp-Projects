9fdb198c9657c6e54ecd6bf87d3e672683f26452
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-09 16:54:38 -0500

    Method Class

---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)//voiding the main method and only showing the second number is what the entire block of code is doing. 
+        {
+            Method method = new Method();//calling the method class from method.cs
+
+            method.Operation(num1: 3, num2: 9);
+
+            Console.ReadLine();
+        }
+    }
+}
