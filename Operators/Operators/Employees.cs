8d08c94b14c1dd0777e6ddbc0f3babba88e14168
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-14 18:41:16 -0500

    Operators

---
+++ Operators/Operators/Employees.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public List<string> FirstName { get; set; }
+        public List<string> LastName { get; set; }
+
+
+
+        public static bool operator ==(Employee id1, Employee id2) // overloading operator ==
+        {
+            return (id1.Id == id2.Id);
+
+        }
+        public static bool operator !=(Employee id1, Employee id2) // overloading operator !=
+        {
+            return (id1.Id != id2.Id);
+
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();  
+            Employee employee2 = new Employee();    
+
+            employee.FirstName = new List<string>() { "David" }; 
+            employee.LastName = new List<string>() { "Goggins" }; 
+            employee.Id = 1;    //add value of 1 to employee property 
+
+
+            employee2.FirstName = new List<string>() { "Mori" }; 
+            employee2.LastName = new List<string>() { "Jinn" }; 
+            employee2.Id = 2;   //add value of 2 to employee2 property
+
+
+
+
+            Console.WriteLine((employee == employee2)); // The result displays if equal, could also return != if not equal 
+
+            Console.ReadLine(); 
+
+        }
+    }
+}
