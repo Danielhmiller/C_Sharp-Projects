09c75be7fcd0a14e3472b208307bdaf83b89862f
Danielhmiller <danielmillerusmc3043@outlook.com>
2023-06-14 18:51:15 -0500

    Parsing Enums

---
+++ Parsing Enums/Parsing Enums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Parsing_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What day of the week is today?");
+                string dayEntered = Console.ReadLine();
+
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), dayEntered);
+
+                if (day == DaysOfTheWeek.Wednesday)
+                {
+                    Console.WriteLine("Today is Wednesday.");
+                }
+                else
+                {
+                    Console.WriteLine("Today is not " + day);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.Read();
+        }
+
+        public enum DaysOfTheWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+    }
+}
