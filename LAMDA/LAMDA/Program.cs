5c6f3492ad7d658c50a7800f8a4ef4535e036a03
Danielhmiller <107880080+Danielhmiller@users.noreply.github.com>
2023-06-14 21:22:18 -0500

    assignment update

--- LAMDA/LAMDA/Program.cs
+++ LAMDA/LAMDA/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LAMBDA
 {
@@ -7,21 +11,21 @@ namespace LAMBDA
         static void Main(string[] args)
         {
             List<Employee> employeeList = new List<Employee> { new Employee(), new Employee(), new Employee(), new Employee(), new Employee(), new Employee(), new Employee(), new Employee(), new Employee(), new Employee() }; // 10 employees
-            employeeList[0].FirstName = "Joe"; 
-            employeeList[0].Id = 5; 
+            employeeList[0].FirstName = "Joe";
+            employeeList[0].Id = 5;
 
-            employeeList[1].FirstName = "David'"; 
-            employeeList[1].Id = 6; 
+            employeeList[1].FirstName = "David'";
+            employeeList[1].Id = 6;
 
-            employeeList[2].FirstName = "Joe"; 
-            employeeList[2].Id = 7; 
+            employeeList[2].FirstName = "Joe";
+            employeeList[2].Id = 7;
 
             List<Employee> joeList = new List<Employee>();
 
             foreach (Employee employee in employeeList)
             {
 
-                if (employee.FirstName == "Daniel")
+                if (employee.FirstName == "Joe")
                 {
                     joeList.Add(employee);
                     //Console.WriteLine(employee.FirstName);  // test result
@@ -29,10 +33,10 @@ namespace LAMBDA
 
             }
 
-            List<Employee> newjoeList = employeeList.Where(x => x.FirstName == "Daniel").ToList();
+            List<Employee> newjoeList = employeeList.Where(x => x.FirstName == "Joe").ToList();
 
             List<Employee> idjoeList = employeeList.Where(x => x.Id > 5).ToList(); // Lambda to return Id greater than 5
-            //Console.WriteLine(iddanielList[0].Id + ", " + iddanielList[1].Id);  // test result
+            //Console.WriteLine(idJOEList[0].Id + ", " + idJOEList[1].Id);  // test result
 
             Console.ReadLine();
         }
