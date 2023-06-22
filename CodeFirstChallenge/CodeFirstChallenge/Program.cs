edda62f6e50e68de906b40a0eed8814ecea9366d
Danielhmiller <107880080+Danielhmiller@users.noreply.github.com>
2023-06-21 19:25:43 -0500

    Final Challenge

---
+++ CodeFirstChallenge/CodeFirstChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var studentCtx = new StudentContext())
+            {
+                var student = new Student() { StudentId = 0, StudentFName = "Daniel", StudentLName = "Miller", StudentGrade = 10 };
+
+                studentCtx.students.Add(student);
+                studentCtx.SaveChanges();
+            }
+        }
+    }
+}
